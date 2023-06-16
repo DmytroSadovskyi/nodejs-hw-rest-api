07fb31a4e7231ed2ec04dce60c08f7121e9d4df7
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-06-16 18:29:46 +0300

    Update contacts.js
    add new route for update favorite status

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,14 +4,18 @@ const router = express.Router();
 
 const ctrl = require("../../controllers/contacts");
 
+const isValidId = require("../../middlewares");
+
 router.get("/", ctrl.listContacts);
 
-router.get("/:id", ctrl.getById);
+router.get("/:id", isValidId, ctrl.getById);
 
 router.post("/", ctrl.addContact);
 
-router.put("/:id", ctrl.updateById);
+router.put("/:id", isValidId, ctrl.updateById);
 
-router.delete("/:id", ctrl.removeContact);
+router.patch("/:id/favorite", isValidId, ctrl.updateStatusContact);
+
+router.delete("/:id", isValidId, ctrl.removeContact);
 
 module.exports = router;
