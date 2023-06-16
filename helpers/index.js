60668fb295207ba8c218a76bb93aa6933d16f8a0
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-06-16 18:28:13 +0300

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,8 @@
 const HttpError = require("./HttpError");
-const ctrl = require("./ctrlWrapper");
+const ctrlWrapper = require("./ctrlWrapper");
+const handleMongooseError = require("./handleMongooseError");
 module.exports = {
   HttpError,
-  ctrl,
+  ctrlWrapper,
+  handleMongooseError,
 };
