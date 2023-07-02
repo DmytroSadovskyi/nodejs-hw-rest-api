c837d4062fbd9ba2dc99a43e3babb8a22c645aa7
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-07-02 18:24:21 +0300

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
 const router = express.Router();
@@ -30,6 +30,13 @@ router.patch(
   authenticate,
   validateBody(schemas.changeSubscriptionSchema),
   ctrlWrapper(ctrl.changeSubscription)
+);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.updateUserAvatar)
 );
 
 module.exports = router;
